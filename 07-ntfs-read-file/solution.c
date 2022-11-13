1fe47e095fbe5fcbbc98459203e8e3fd54932703
VladimirVolodya <smirvovn@gmail.com>
2022-11-13 20:48:08 +0300

    custom pathname_to_inode

--- 07-ntfs-read-file/solution.c
+++ 07-ntfs-read-file/solution.c
@@ -17,6 +17,8 @@ int fd_to_path(int fd, char *out);
 int unmount(ntfs_volume *p_ntfs_volume);
 int ntfs_read(ntfs_attr *p_ntfs_attr, int out, u32 blk_sz);
 int ntfs_read_mst(ntfs_attr *p_ntfs_attr, int out, u32 blk_sz);
+ntfs_inode *ntfs_pathname_to_inode_enotdir(ntfs_volume *vol, ntfs_inode *parent,
+                                           const char *pathname);
 
 int dump_file(int img, const char *path, int out) {
   char img_file[PATH_MAX];
@@ -127,4 +129,113 @@ int ntfs_read_mst(ntfs_attr *p_ntfs_attr, int out, u32 blk_sz) {
     }
     offset += read_bytes;
   }
+}
+
+ntfs_inode *ntfs_pathname_to_inode_enotdir(ntfs_volume *vol, ntfs_inode *parent,
+                                           const char *pathname) {
+  u64 inum;
+  int len, err = 0;
+  char *p, *q;
+  ntfs_inode *ni;
+  ntfs_inode *result = NULL;
+  ntfschar *unicode = NULL;
+  char *ascii = NULL;
+
+  if (!vol || !pathname) {
+    errno = EINVAL;
+    return NULL;
+  }
+
+  ntfs_log_trace("path: '%s'\n", pathname);
+
+  ascii = strdup(pathname);
+  if (!ascii) {
+    ntfs_log_error("Out of memory.\n");
+    err = ENOMEM;
+    goto out;
+  }
+
+  p = ascii;
+  /* Remove leading /'s. */
+  while (p && *p && *p == PATH_SEP) p++;
+  if (parent) {
+    ni = parent;
+  } else {
+    ni = ntfs_inode_open(vol, FILE_root);
+    if (!ni) {
+      ntfs_log_debug(
+          "Couldn't open the inode of the root "
+          "directory.\n");
+      err = EIO;
+      result = (ntfs_inode *)NULL;
+      goto out;
+    }
+  }
+
+  while (p && *p) {
+    /* Find the end of the first token. */
+    q = strchr(p, PATH_SEP);
+    if (q != NULL) {
+      *q = '\0';
+    }
+    len = ntfs_mbstoucs(p, &unicode);
+    if (len < 0) {
+      ntfs_log_perror(
+          "Could not convert filename to Unicode:"
+          " '%s'",
+          p);
+      err = errno;
+      goto close;
+    } else if (len > NTFS_MAX_NAME_LEN) {
+      err = ENAMETOOLONG;
+      goto close;
+    }
+    inum = ntfs_inode_lookup_by_name(ni, unicode, len);
+    if (inum == (u64)-1) {
+      ntfs_log_debug(
+          "Couldn't find name '%s' in pathname "
+          "'%s'.\n",
+          p, pathname);
+      err = ENOENT;
+      goto close;
+    }
+
+    if (ni != parent)
+      if (ntfs_inode_close(ni)) {
+        err = errno;
+        goto out;
+      }
+
+    inum = MREF(inum);
+    ni = ntfs_inode_open(vol, inum);
+    if (!ni) {
+      ntfs_log_debug("Cannot open inode %llu: %s.\n", (unsigned long long)inum,
+                     p);
+      err = EIO;
+      goto close;
+    }
+
+    free(unicode);
+    unicode = NULL;
+
+    if (q) {
+      *q++ = PATH_SEP; /* JPA */
+    } else if (!(ni->mrec->flags & MFT_RECORD_IS_DIRECTORY)) {
+      err = ENOTDIR;
+      goto close;
+    }
+    p = q;
+    while (p && *p && *p == PATH_SEP) p++;
+  }
+
+  result = ni;
+  ni = NULL;
+close:
+  if (ni && (ni != parent))
+    if (ntfs_inode_close(ni) && !err) err = errno;
+out:
+  free(ascii);
+  free(unicode);
+  if (err) errno = err;
+  return result;
 }
